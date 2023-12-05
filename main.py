e9f88d73257e372545989afd4bda007bebdb9a5c
otter8sky <sofiia.klementieva@gmail.com>
2023-12-05 13:15:05 +0300

    added beeping and printing execution time

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import pandas as pd
 from comp import comp
 from comp import read_data_file
 from comp import read_comp_file
@@ -15,6 +17,9 @@ from methods import By_Leap_Frog
 from methods import methods
 from methods import methods_names
 from operations import *
+import winsound
+
+
 
 Methods = [By_Ex_Euler,
            By_PC,
@@ -22,11 +27,16 @@ Methods = [By_Ex_Euler,
            By_Verlet,
            By_Leap_Frog]
 
+start_time = time.perf_counter()
+
 bodies = read_data_file('DATA.txt')
 # problem = read_comp_file('COMPUTING.txt')
 
+freq = 500
+dur = 800
+
 method = 'By_Leap_Frog'
-time_end = 8
+time_end = 100
 initial_timestep = 0.001
 delta_vel = 0.001
 delta_coord = 0.001
@@ -36,12 +46,20 @@ timestep_min = 0.0000001
 problem = Problem(get_method(method, methods, methods_names), time_end, initial_timestep,
                   delta_vel, delta_coord, delta_timestep, timestep_max, timestep_min)
 
-# clear_all_datafiles(bodies)
-# comp(problem, bodies)
+clear_all_datafiles(bodies)
+comp(problem, bodies)
 
 # plot_bodies(bodies, problem)
 # plot_energy(problem)
 # plot_momentum(problem)
 # plot_cm(problem)
 # plot_time_step(problem)
+
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+print_ex_time(execution_time)
+
+winsound.Beep(freq, dur)
+
 plot_all(problem, bodies)
+
