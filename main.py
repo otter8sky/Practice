99a34597511a40c1ba75b3229a4c70544696184f
otter8sky <sofiia.klementieva@gmail.com>
2024-01-22 00:14:27 +0300

    added dt_output, printing execution time

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from comp import plot_cm
 from comp import plot_momentum
 from comp import plot_time_step
 from comp import plot_all
+from comp import plot_angular_momentum
+from comp import plot_elements
 from methods import By_Ex_Euler
 from methods import By_PC
 from methods import By_RK_4
@@ -30,36 +32,39 @@ Methods = [By_Ex_Euler,
 
 start_time = time.perf_counter()
 
-bodies = read_data_file('data2.txt')
+bodies = read_data_file('data_correct.txt')
 # problem = read_comp_file('COMPUTING.txt')
 
-freq = 500
-dur = 800
-
-method = 'By_RK_N'
-time_end = 1
+method = 'By_Leap_Frog'
+time_end = 10
 initial_timestep = 0.0001
 delta_vel = 0.001
 delta_coord = 0.0001
 delta_timestep = 0.0000001
 timestep_max = 0.01
 timestep_min = 0.0000001
+dt_output = 0.01
 problem = Problem(get_method(method, methods, methods_names), time_end, initial_timestep,
-                  delta_vel, delta_coord, delta_timestep, timestep_max, timestep_min)
+                  delta_vel, delta_coord, delta_timestep, timestep_max, timestep_min, dt_output)
 
 clear_all_datafiles(bodies)
 comp(problem, bodies)
 
+end_time = time.perf_counter()
+execution_time = end_time
+print_ex_time(execution_time)
+#
 # plot_bodies(bodies, problem)
+plot_elements(bodies, problem)
 # plot_energy(problem)
 # plot_momentum(problem)
 # plot_cm(problem)
 # plot_time_step(problem)
+# plot_angular_momentum(problem)
 
-# end_time = time.perf_counter()
-# execution_time = end_time - start_time
-# print_ex_time(execution_time)
-#
+# freq = 500
+# dur = 1000
 # winsound.Beep(freq, dur)
-#
-plot_all(problem, bodies)
+
+# plot_all(problem, bodies)
+
